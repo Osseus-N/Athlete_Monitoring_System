e87231ff38c8bb93b63313948e9db3abbaea9f2c
NORLITO DUMALE DOMINGO <147986845+Osseus-N@users.noreply.github.com>
2026-01-10 17:58:38 +0800

    Remove hardcoded database credentials
    Removed sensitive database credentials from the code.

--- src/database/DatabaseConnection.java
+++ src/database/DatabaseConnection.java
@@ -8,9 +8,9 @@ import java.sql.*;
 public class DatabaseConnection {
     
   
-    private static final String URL = "jdbc:mysql://localhost:3306/athlete_db";
-    private static final String USER = "root";
-    private static final String PASSWORD = "Ndomingo30!"; 
+    private static final String URL = "";
+    private static final String USER = "";
+    private static final String PASSWORD = ""; 
 
     public static Connection getConnection() {
         Connection con = null;
